23fc582dc9ffc68f6effb502ccce943b8df4b23f
BharathHU <bharathhubharath@gmail.com>
2026-04-08 11:02:13 +0530

    updated

---
+++ js operators/Arithmetic.js
@@ -0,0 +1,22 @@
+var a=10
+var b=20
+console.log("Sum of num is:",a+b)
+console.log("Diffrence b/w num is:",a-b)
+console.log("Product of num is:",a*b)
+console.log("Exponentiation of num is:",a**b)
+console.log("Division of num is:",a/b)
+let pos=4
+let neg=-4
+console.log(pos-neg)
+console.log(pos*2)
+console.log(20%10)
+console.log(1+"3")
+console.log(1-"3")
+console.log(1*"3")
+console.log("1"+3)
+console.log("1"-3)
+console.log(1+"1"+2+2+"3")
+console.log("11"+2+2+"3")
+console.log("5"*[])
+console.log("5"*[10])
+console.log("5"*[2,3])
