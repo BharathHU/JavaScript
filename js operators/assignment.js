68b70c577f1b8eb675364431c01a1c521b80032f
BharathHU <bharathhubharath@gmail.com>
2026-04-08 11:58:09 +0530

    updated

---
+++ js operators/assignment.js
@@ -0,0 +1,25 @@
+let num = 5;
+console.log(num++);   // post-increment
+console.log(num);
+console.log(++num);   // pre-increment
+
+let a = 200;
+console.log(a--);     // post-decrement
+console.log(a);       // ❌ fixed typo here
+console.log(--a);     // pre-decrement
+
+var number = 10;
+number *= 2;
+console.log(number);
+
+number /= 2;
+console.log(number);
+
+number %= 2;
+console.log(number);
+
+number += 2;
+console.log(number);
+
+number -= 2;
+console.log(number);
