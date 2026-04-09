d806fbd5c394a8a30d01100e2b6014046754d45b
BharathHU <bharathhubharath@gmail.com>
2026-04-09 10:59:48 +0530

    updated

---
+++ js functions/func.js
@@ -0,0 +1,13 @@
+function greet(){
+    console.log("HIII")
+    console.log("Welcome to JS class")
+}
+greet()
+function add(){
+    let a=10
+    let b=20
+    let res=a+b
+    console.log(res)
+
+}
+add()
