fc9319cbd527d65815065d73618a757b5b785c48
BharathHU <bharathhubharath@gmail.com>
2026-04-09 11:49:08 +0530

    updated

---
+++ js functions/return_fun.js
@@ -0,0 +1,19 @@
+function add(){
+    let a=100
+    let b=200
+    let c=a*b
+}
+console.log(add())
+function great(){
+    console.log("HIII,Welcome!!!")
+    return great
+}
+great()
+function add(x,y){
+    return c=x+y
+}
+console.log(add(10,2))
+function xx(name){
+    return "Hello" + name
+}
+console.log(xx("Bharath"))
