19366deb437525460dce234fa80557dfab807642
BharathHU <bharathhubharath@gmail.com>
2026-04-09 11:29:17 +0530

    updated

---
+++ js functions/named_fun.js
@@ -0,0 +1,11 @@
+// function --> call --> o/p
+function multiple(){
+    var a=10
+    var b=2
+    console.log(a*b)
+    let a1=50
+    let b1=40
+    let res=(a1/b1)
+    console.log(res)
+}
+multiple()
---
+++ js functions/parameterized_fun.js
@@ -0,0 +1,16 @@
+function add(a,b){
+    let c=a+b
+    console.log(c)
+    let c1=a*b
+    console.log(c1)
+}
+add(10,20)
+function data(name,age,city){
+console.log("My name is:",name)
+console.log("My age was:",age)
+console.log(name,"lived in",city)
+}
+data("Bharath",21,"Shivamogga")
+data("Vishal",56,"Bidar")
+data("Ujju",60,"Naga's nagar")
+data("Chaluva",93,"Bapuji_nagar")
