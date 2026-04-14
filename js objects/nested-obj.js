b2b885c25643283bdd354497fbbda8aea34db605
BharathHU <bharathhubharath@gmail.com>
2026-04-14 12:14:39 +0530

    updated

---
+++ js objects/nested-obj.js
@@ -0,0 +1,18 @@
+var user={
+    name:"BHarath",
+    id:101,
+    email:"bharathhubharath@gmail.com",
+    area:"Nagarabhavi",
+    city:"Bengaluru",
+    skills:"python,Front-end",
+    area:{
+        doorno:7,
+        street:"1st main,xyzzzzzzzzz",
+        cross:"4th cross"
+    }
+}
+console.log(user)
+user.name="Sharath"
+user.area.doorno=10
+console.log(user)
+
