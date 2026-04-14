7b1f1a9c594ea8da9fb53ebc9d5156ab472d3d85
BharathHU <bharathhubharath@gmail.com>
2026-04-14 12:04:17 +0530

    updated

---
+++ js objects/insert-addOp.js
@@ -0,0 +1,13 @@
+let product={
+    pname:"I phone",
+    price:120000,
+    rate:4.5,
+    stock:true,
+    cotegory:"Electronics"
+}
+console.log(product)
+
+//insert() or add()
+product.brand="apple"
+product['color']="White"
+console.log(product)
