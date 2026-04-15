91b01938c600dcf66d68f521df097d0853e260cb
BharathHU <bharathhubharath@gmail.com>
2026-04-15 11:51:33 +0530

    updated

---
+++ js objects/objectfreeze.js
@@ -0,0 +1,13 @@
+//Object.freeze()
+// # object.freeze(object_name)
+// # obeject.freeze() is a method used to freeze an object
+// # you cannot add new properties
+// # you cannot remove existing properties
+// # you cannot change the values of existing properties
+// # you cannot reassign or reconfigure any property
+const user1={name:"Raj",age:21}
+// Object.freeze(user1)
+user1.name="Bharath"
+user1.city="Mumbai"
+delete user1.age
+console.log(user1)
