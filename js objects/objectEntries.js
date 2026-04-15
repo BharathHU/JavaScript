26e71c6d3b914e3c13f2f720a2fbf39861c3bb6d
BharathHU <bharathhubharath@gmail.com>
2026-04-15 11:36:00 +0530

    updated

---
+++ js objects/objectEntries.js
@@ -0,0 +1,13 @@
+let target={
+    name:"bharath",
+    age:23,
+    gender:"Male"
+}
+let source={
+    country:"India",
+    occupation:"Development"
+}
+console.log(target)
+console.log(source)
+res=Object.entries(target,source)
+console.log(res)
