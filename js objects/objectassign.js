e9fdf1152acefa491141e26080c5f8cc8071fd2e
BharathHU <bharathhubharath@gmail.com>
2026-04-15 11:33:46 +0530

    updated

---
+++ js objects/objectassign.js
@@ -0,0 +1,13 @@
+let target={
+    name:"bharath",
+    age:23,
+    gender:"Male"
+}
+let source={
+    country:"India",
+    occupation:"Development"
+}
+console.log(target)
+console.log(source)
+res=Object.assign(target,source)
+console.log(res)
