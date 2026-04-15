d14dba9a864fe53a390a3a94da0575717d1487f5
BharathHU <bharathhubharath@gmail.com>
2026-04-15 12:12:47 +0530

    updated

---
+++ js Arrays/array-literals.js
@@ -0,0 +1,6 @@
+// Syntax:
+// var arr_name=[]
+// let
+// const
+var arr=[10,true,"email",{city:"Bengaluru",pin:560023},"Male"]
+console.log(arr)
---
+++ js Arrays/arrayContruct.js
@@ -0,0 +1,8 @@
+// syntax:
+// var array_name=new Array()
+// or 
+// let
+// or
+// const
+var arr=new Array("raj","true",100,56,70,false)
+console.log(arr)
