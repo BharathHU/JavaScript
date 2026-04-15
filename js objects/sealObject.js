e7efaeb970b98f3032f6de898b3279c78877b050
BharathHU <bharathhubharath@gmail.com>
2026-04-15 11:27:10 +0530

    updated

--- js objects/sealObject.js
+++ js objects/sealObject.js
@@ -1,9 +1,14 @@
 let user={
     name:"Bharath",
-    age:22
+    age:22,
+    xyz:"abc"
 }
 Object.seal(user)
 console.log(user.age)
-console.log(user)
+console.log(user) //allowed we can access 
 user.city="Mysuru"
-console.log(user)
+console.log(user)//not allowed
+// {
+//     name:"kaka",  //not allowed
+//     age:33  // we can't modify or we can't insert
+// }
