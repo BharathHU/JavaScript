3120e8b7094d32d8dd714b6aaa9b5829e61b862b
BharathHU <bharathhubharath@gmail.com>
2026-04-16 11:30:59 +0530

    updated

---
+++ js Arrays/arrayMethods.js
@@ -0,0 +1,36 @@
+// 1.push()
+// 2.pop()
+// 3.unshift()
+// 4.shift()
+// 5.indexOf()
+// 6.lastindexOf()
+// 7.join()
+// 8.includes()
+// 9.reverse()
+// 10.slice()
+// 11.splice()
+//==================================================================================================================================
+ //1.push()
+ console.log("push()")
+
+ var arr=[]
+ arr.push(10)
+ console.log(arr)
+ arr.push("Bharath")
+ console.log(arr)
+ arr.push(true)
+ console.log(arr)
+ arr.push(143)
+ console.log(arr)
+console.log("=========================================================")
+
+ //2.pop()
+ console.log("pop()")
+
+ var arr1=[10,"Sachin",true,15.6789,"Dhoni","y"]
+ console.log(arr1)
+ arr1.pop()
+ console.log(arr1)
+ arr1.pop()
+ console.log(arr1)
+
