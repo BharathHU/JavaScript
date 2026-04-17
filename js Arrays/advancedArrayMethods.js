7f8d5cb6b1fda2b953fcb7d992036223a9c80335
BharathHU <bharathhubharath@gmail.com>
2026-04-17 11:08:24 +0530

    updated

---
+++ js Arrays/advancedArrayMethods.js
@@ -0,0 +1,20 @@
+// forEach()
+// we can access every elements in array
+console.log("forEach()")
+var arr=["raj",100,"rahul",true]
+arr.forEach(function(ele,index){
+    console.log(ele,index)
+})
+console.log(arr)
+//==================================================
+let arr1=['Bharath',100,true,200,777,false,'raj']
+arr1.forEach(function(ele,index){
+    console.log(ele + " " + index)
+})
+let data=[{name:"Bharath",age:22},
+    {name:"Nischitha",age:22},
+{name:"Nibha",age:2}
+]
+data.forEach(function(ele,index){
+console.log(data)
+})
