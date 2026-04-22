4b829bf2ee2858d35a5780b2010301d297895f5e
BharathHU <bharathhubharath@gmail.com>
2026-04-22 11:00:27 +0530

    updated

---
+++ JSON/StringMethods.js
@@ -0,0 +1,22 @@
+//1.charAt() , 2.toLowerCase(), 3.toUpperCase(), 4.replaceAll(), 5.slice(), 6.substring(), 8.replace(), 9.trim()
+// charAt()
+console.log("charAt():")
+let str="Bharath"
+console.log(str.length)
+console.log(str.charAt())
+console.log(str.charAt(2))
+//=======================================================================
+//toLowerCase()
+console.log("toLowerCase():")
+let str1="BhArAtH"
+console.log(str1.toLowerCase())
+//=======================================================================
+//toUpperCase()
+console.log("toUpperCase():")
+let str2="BhArAtH"
+console.log(str2.toUpperCase())
+//=======================================================================
+//SubString()
+console.log("SubString():")
+let str3="Welcome to JavaScript"
+console.log(str3.substring(3, 21))
